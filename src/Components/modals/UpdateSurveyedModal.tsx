ffd9bd7093ff77be69338f9b4969f00eb879ece2
Masp2601 <masp2601@gmail.com>
2023-09-07 16:28:23 -0500

    add neigborhoods value

--- src/Components/modals/UpdateSurveyedModal.tsx
+++ src/Components/modals/UpdateSurveyedModal.tsx
@@ -1,104 +1,134 @@
-import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { useCustomDispatch, useCustomSelector } from '../../redux/hooks/useRedux';
-import { setOpenModalUpdateRespondent } from '../../redux/slices/surveyedSlice';
-import { useEffect, useState } from 'react';
-import { fetchMunicipalities } from '../../redux/thunks/municipalitiesThunk';
-import { fetchNeighborhoods } from '../../redux/thunks/neighborhoodsThunk';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import {
+  useCustomDispatch,
+  useCustomSelector,
+} from "../../redux/hooks/useRedux";
+import { setOpenModalUpdateRespondent } from "../../redux/slices/surveyedSlice";
+import { useEffect, useState } from "react";
+import { fetchMunicipalities } from "../../redux/thunks/municipalitiesThunk";
+import { fetchNeighborhoods } from "../../redux/thunks/neighborhoodsThunk";
 
 export const UpdateSurveyedModal = () => {
   const dispatch = useCustomDispatch();
-  const { openModalUpdateRespondent, respondent } = useCustomSelector((state) => state.surveyed);
-  const [selectedMunicipalityId, setSelectedMunicipalityId] = useState(respondent?.neighborhood.municipality.id);
+  const { openModalUpdateRespondent, respondent } = useCustomSelector(
+    (state) => state.surveyed
+  );
+  const [selectedMunicipalityId, setSelectedMunicipalityId] = useState(
+    respondent?.neighborhood.municipality.id
+  );
   const { municipalities } = useCustomSelector((state) => state.municipalities);
   const { neighborhoods } = useCustomSelector((state) => state.neighborhoods);
 
-  const idMunicipality = selectedMunicipalityId === undefined ? respondent?.neighborhood.municipality.id : selectedMunicipalityId;
+
+  const idMunicipality =
+    selectedMunicipalityId === undefined
+      ? respondent?.neighborhood.municipality.id
+      : selectedMunicipalityId;
 
   useEffect(() => {
-    dispatch(fetchMunicipalities())
-    dispatch(fetchNeighborhoods(idMunicipality))
-  }, [dispatch, idMunicipality])
+    dispatch(fetchMunicipalities());
+    dispatch(fetchNeighborhoods(idMunicipality));
+  }, [dispatch, idMunicipality]);
 
   return (
-    <Modal
-      isOpen={openModalUpdateRespondent}
-    >
+    <Modal isOpen={openModalUpdateRespondent}>
       <ModalHeader>actualizar encuestado</ModalHeader>
       <ModalBody>
-        <form className='d-flex flex-column gap-3'>
+        <form className="d-flex flex-column gap-3">
           <input
-            type='text'
-            className='form-control'
-            placeholder='nombre'
+            type="text"
+            className="form-control"
+            placeholder="nombre"
             defaultValue={respondent?.name}
           />
           <input
-            type='text'
-            className='form-control'
-            placeholder='telefono'
+            type="text"
+            className="form-control"
+            placeholder="telefono"
             defaultValue={respondent?.phoneNumber}
           />
           <input
-            type='text'
-            className='form-control'
-            placeholder='cedula'
+            type="text"
+            className="form-control"
+            placeholder="cedula"
             defaultValue={respondent?.identityCard}
           />
           <select
             defaultValue={respondent?.neighborhood.municipality.id}
-            className='form-control'
-            onChange={(e) => setSelectedMunicipalityId(parseInt(e.target.value))}
+            className="form-control"
+            onChange={(e) =>
+              setSelectedMunicipalityId(parseInt(e.target.value))
+            }
           >
-            {municipalities && municipalities.map((municipality) => (
-              <option key={municipality.id} value={municipality.id}>{municipality.name}</option>
-            ))}
+            {municipalities &&
+              municipalities.map((municipality) => (
+                <option key={municipality.id} value={municipality.id}>
+                  {municipality.name}
+                </option>
+              ))}
           </select>
-          <select
-            defaultValue={respondent?.neighborhood.id}
-            className='form-control'
-            onChange={(e) => console.log(e.target.value)}
-          >
-            {neighborhoods && neighborhoods.map((neighborhood) => {
-              return (
-                <option key={neighborhood.id} value={neighborhood.id}>{neighborhood.name}</option>
-              )
-            })}
-          </select>
+            <select
+              defaultValue={respondent?.neighborhood.id || ""}
+              className="form-control"
+              onChange={(e) => console.log(e.target.value)}
+            >
+              {municipalities && selectedMunicipalityId ? (
+              municipalities.find(
+                  (municipality) => municipality.id === selectedMunicipalityId
+                )
+                ?.neighborhoods.map((neighborhood) => (
+                  <option key={neighborhood.id} value={neighborhood.id}>
+                    {neighborhood.name}
+                  </option>
+                ))): (<option value={respondent?.neighborhood.id}>Seleccione un barrio</option>)
+              }
+            </select>
+            {/*{neighborhoods.length &&
+              neighborhoods.map((neighborhood) => {
+                return (
+                  <option key={neighborhood.id} value={neighborhood.id}>
+                    {neighborhood.name}
+                  </option>
+                );
+              })}
+            </select>*/}
           <input
-            type='text'
-            className='form-control'
-            placeholder='direccion'
+            type="text"
+            className="form-control"
+            placeholder="direccion"
             defaultValue={respondent?.address}
           />
           <select
-            defaultValue={respondent?.votingTable.pollingStation.votingMunicipality.name}
-            className='form-control'
+            defaultValue={
+              respondent?.votingTable.pollingStation.votingMunicipality.name
+            }
+            className="form-control"
           >
             <option></option>
           </select>
           <select
             defaultValue={respondent?.votingTable.pollingStation.name}
-            className='form-control'
+            className="form-control"
           >
             <option></option>
           </select>
           <select
             defaultValue={respondent?.votingTable.name}
-            className='form-control'
+            className="form-control"
           >
             <option></option>
           </select>
         </form>
       </ModalBody>
       <ModalFooter>
-        <button className='btn btn-primary'>actualizar</button>
+        <button className="btn btn-primary">actualizar</button>
         <button
-          className='btn btn-danger'
+          className="btn btn-danger"
           onClick={() => dispatch(setOpenModalUpdateRespondent(false))}
         >
           cancelar
         </button>
       </ModalFooter>
     </Modal>
-  )
+  );
 };
