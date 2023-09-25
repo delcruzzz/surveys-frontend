2f804f7510cc9516de25a307306570236d0de0b5
leonardo de la cruz <leonardodelacruz03@hotmail.com>
2023-09-25 17:02:14 -0500

    finish validRole checked authorized routes

--- src/Auth/RoleProtectedRoute.tsx
+++ src/Auth/RoleProtectedRoute.tsx
@@ -7,12 +7,14 @@ const RoleProtectedRoute = (props:any) => {
 
   try {
     const decode: AuthJWTResponse = jwt_decode(token);
-    const validRole = decode.roles.find((role) => role.match('/SUPERADMIN'));
+    const validRole = decode.roles.find((role) => role === 'SUPERADMIN');
+    console.table({roles: decode.roles})
+    console.table(validRole)
 
-    if (validRole && token) {
+    if (validRole) {
       return <><Route {...props} /></>
     }
-    return <Redirect to="*" />;
+    return <Redirect to="/" />;
   } catch (error) {
     return <Redirect to="*" />;
   }
