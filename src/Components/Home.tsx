40f589cba98e21e830a6bfa17aeeb90ad9e2e907
Masp2601 <masp2601@gmail.com>
2023-09-25 14:35:39 -0500

    add setTimeout in home

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,60 +1,70 @@
-import { FC } from 'react';
-import './home.css';
-import { Link, useHistory } from 'react-router-dom';
-import { TableSurveyed } from './TableSurveyed';
-import { AuthUserResponse } from '../redux/interfaces/authInterface';
-import { useCustomDispatch } from '../redux/hooks/useRedux';
-import { setOpenModalCreateRespondent } from '../redux/slices/surveyedSlice';
-import { CreateSurveyedModal } from './modals/CreateSurveyedModal';
+import { FC } from "react";
+import "./home.css";
+import { Link, useHistory } from "react-router-dom";
+import { TableSurveyed } from "./TableSurveyed";
+import { AuthUserResponse } from "../redux/interfaces/authInterface";
+import { useCustomDispatch } from "../redux/hooks/useRedux";
+import { setOpenModalCreateRespondent } from "../redux/slices/surveyedSlice";
+import { CreateSurveyedModal } from "./modals/CreateSurveyedModal";
 
 const Home: FC<JSX.Element[]> = () => {
   const dispatch = useCustomDispatch();
   const history = useHistory();
-  const userLogged: AuthUserResponse = JSON.parse(localStorage.getItem('user') || '{}');
+  const userLogged: AuthUserResponse = JSON.parse(
+    localStorage.getItem("user") || "{}"
+  );
 
   const logout = () => {
     localStorage.clear();
-    history.push('/login');
+    history.push("/login");
   };
 
-  const validRole = userLogged.roles.find((role) => role.name === 'SUPERADMIN');
+  setTimeout(function () {
+    localStorage.clear();
+    history.push("/login");
+  }, 300000);
+
+  const validRole = userLogged.roles.find((role) => role.name === "SUPERADMIN");
 
   return (
     <>
       <header
         style={{
-          display: 'flex',
-          justifyContent: 'space-between',
+          display: "flex",
+          justifyContent: "space-between",
           paddingLeft: 50,
           paddingRight: 50,
         }}
       >
         <div>
-          <h2 className='m-3 '>
+          <h2 className="m-3 ">
             <p style={{ fontSize: 18 }}>
-              <b>Líder: {userLogged.name}, Télefono: {userLogged.cellPhoneNumber}, CC: {userLogged.identityCard}</b>
+              <b>
+                Líder: {userLogged.name}, Télefono: {userLogged.cellPhoneNumber}
+                , CC: {userLogged.identityCard}
+              </b>
             </p>
           </h2>
         </div>
         <div>
-          <button type='submit' className='butn' onClick={logout}>
+          <button type="submit" className="butn" onClick={logout}>
             Cerrar Sesión
           </button>
         </div>
       </header>
-      <main className='content-main'>
-        <button 
-          className='btn btn-success mb-3'
+      <main className="content-main">
+        <button
+          className="btn btn-success mb-3"
           onClick={() => dispatch(setOpenModalCreateRespondent(true))}
         >
           Agregar Votante
         </button>
-        
-        { 
-          validRole 
-            ? <Link to='/signup' className='btn btn-primary mb-3 mx-3'>Crear usuario</Link>
-            : null  
-        }
+
+        {validRole ? (
+          <Link to="/signup" className="btn btn-primary mb-3 mx-3">
+            Crear usuario
+          </Link>
+        ) : null}
         <TableSurveyed />
       </main>
       <CreateSurveyedModal />
