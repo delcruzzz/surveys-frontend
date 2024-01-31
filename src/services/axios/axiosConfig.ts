433d2dd1132e3e2bdb460a0af70862f5e9d42616
leonardo de la cruz <leonardodelacruz03@hotmail.com>
2024-01-31 11:48:20 -0500

    fixed axiosConfig

--- src/services/axios/axiosConfig.ts
+++ src/services/axios/axiosConfig.ts
@@ -11,7 +11,7 @@ const apiConfig = axios.create({
 });
 
 apiConfig.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('auth');
   if (token) {
     config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
